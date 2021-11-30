2824d5581163f01fda73ddd279befe4bfe427a9a
koji <koji4386@gmail.com>
2021-11-30 17:01:49 +0100

    add tour, tours component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tours-project'
+
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+
+  const getTours = async () => {
+    setLoading(true)
+    try {
+      await fetch(url)
+        .then(res => res.json())
+        .then(result => {
+          setTours(result)
+          setLoading(false)
+          console.log(result)
+        });
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  const removeTour = (id) => {
+    const newTours = tours.filter(tour => tour.id !== id)
+    setTours(newTours)
+  }
+  
+  useEffect(() => {
+    getTours()
+  }, [])
+
+  if(loading){
+    return (
+      <main>
+        <Loading />
+       </main>
+    )
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
 export default App
